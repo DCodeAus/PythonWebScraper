618369551f0d20d90fc1b4cb0b5b5d3a745dd22d
DCodeAus <>
2022-06-24 15:30:00 +1000

    Python web scraping formatting jobs output nicely

--- PythonWebScraper/Webscraper.py
+++ PythonWebScraper/Webscraper.py
@@ -16,5 +16,16 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 #finds the specific html element by its ID, in this case Results Container
 results = soup.find(id="ResultsContainer")
+#searching the above results, for card content
+job_elements=results.find_all("div", class_="card-content")
+for job_element in job_elements:
+    title_element=job_element.find("h2",class_="title")
+    company_element=job_element.find("h3",class_="company")
+    location_element=job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print ()
+    #print(job_element, end= "\n"*2)
 #prints the text
-print(results.prettify())
+#print(results.prettify())
